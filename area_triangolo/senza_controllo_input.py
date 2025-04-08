ad333286fb12bd5cee4e0e9048cd6d4206a7cce6
AbysmalWhale <amongus1083737@gmail.com>
2025-04-08 22:02:21 +0200

    Added homework on "area del triangolo" without input check

---
+++ area_triangolo/senza_controllo_input.py
@@ -0,0 +1,9 @@
+# <redacted>
+# 26/03/2025
+
+base = int(input("Inserisci la base del triangolo (in cm): "))
+altezza = int(input("Inserisci l'altezza del triangolo (in cm): "))
+
+area = (base * altezza) / 2
+
+print("L'area del triangolo è", area, "cm quadrati")
